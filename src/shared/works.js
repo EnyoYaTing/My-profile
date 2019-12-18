506a1ac72f10c52c3cbbe7fbb9cd633ed932589f
Ya-Ting Lin <da18787@bristol.ac.uk>
2019-12-18 19:17:42 +0000

    add HomeComponent

---
+++ src/shared/works.js
@@ -0,0 +1,20 @@
+export const WORKS = [
+    {
+        id:0,
+        name: 'WisdomWin Quant Technology',
+        image:'/assets/images/logo.png',
+        title: 'UI designer & Front-end Developer Part-time Job',
+        date: 'Aug 2013 - May 2016',
+        location: 'Taipei, Taiwan',
+        description:'Collaborated with senior Full-stack developers to develop an interactive finance website, which provides users knowledge about Stock, Futures, and Options market.'
+    },
+    {
+        id:1,
+        name: 'Taiwan International Patent & Law Office',
+        image:'/assets/images/homeig.png',
+        title: 'Japanese Patent Law Assistant',
+        date: 'Aug 2013 - May 2016',
+        location: 'Taipei, Taiwan',
+        description: 'Assisted Japanese companies and customers in succeeding in applying for the patent right in Taiwan, including Invention Patent, Utility Model Patent and Design Patent.'
+    }
+]

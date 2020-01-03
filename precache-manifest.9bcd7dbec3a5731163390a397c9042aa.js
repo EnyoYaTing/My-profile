9eee28f4c41e7cc3102267c704df9753fbe97ea1
Ya-Ting Lin <da18787@bristol.ac.uk>
2020-01-03 14:09:33 +0000

    Updates

---
+++ precache-manifest.9bcd7dbec3a5731163390a397c9042aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c0c23a30b4fe9a1dbea927d6a24ecb",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "320e4fa3be970bd0c892",
+    "url": "/my-profile/static/css/2.a9ba06ab.chunk.css"
+  },
+  {
+    "revision": "399ab766fb82ce3c5a80",
+    "url": "/my-profile/static/css/main.ad1c53fe.chunk.css"
+  },
+  {
+    "revision": "320e4fa3be970bd0c892",
+    "url": "/my-profile/static/js/2.2302e35d.chunk.js"
+  },
+  {
+    "revision": "a1a6f866917c0a6b61a32fd8d5e79a9e",
+    "url": "/my-profile/static/js/2.2302e35d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "399ab766fb82ce3c5a80",
+    "url": "/my-profile/static/js/main.e7f3c2e0.chunk.js"
+  },
+  {
+    "revision": "194ab9b62ea6371d1205",
+    "url": "/my-profile/static/js/runtime-main.2647684f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-profile/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my-profile/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-profile/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-profile/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-profile/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);

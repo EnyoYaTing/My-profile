eb12251429765588a5b488998e9e8313e9f9dd3b
Enyo Lin <da18787@alumni.bristol.ac.uk>
2020-01-06 14:52:41 +0000

    image path and home component fuxed

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -6,30 +6,29 @@ class Home extends Component {
     render() {
         return(
             <div className="homepage" id="home">
-                <Jumbotron> </Jumbotron>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-12 col-sm-2 offset-sm-1"></div>
-                            <div className="col-12 col-sm-5 offset-sm-1 "> 
-                                <p> Hi, I'm </p> 
-                            </div>    
+                <img src="./assets/images/homeig.png?raw=true" alt="Enyo" width="100%"/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-sm-2 offset-sm-1"></div>
+                        <div className="col-12 col-sm-5 offset-sm-1 "> 
+                            <p> Hi, I'm </p> 
+                        </div>    
+                    </div> 
+                    <div className="row">
+                        <div className="col-12 col-sm-4"></div>
+                        <div className="col-12 col-sm-5"> 
+                            <div className="header">
+                                <p> Enyo </p>
+                            </div>  
                         </div> 
-                        <div className="row">
-                            <div className="col-12 col-sm-4"></div>
-                            <div className="col-12 col-sm-5"> 
-                                <div className="header">
-                                    <p> Enyo </p>
-                                </div>  
-                            </div> 
-                        </div>       
-                        <div className="row">
-                            <div className="col-12 col-sm-1 offset-sm-1"></div>   
-                            <div className="col-12 col-sm-9 ">      
-                                <p> On the way towards a professional Web Developer.</p>
-                            </div> 
-                        </div>
-                       
+                    </div>       
+                    <div className="row">
+                        <div className="col-12 col-sm-1 offset-sm-1"></div>   
+                        <div className="col-12 col-sm-9 ">      
+                            <p> On the way towards a professional Web Developer.</p>
+                        </div> 
                     </div>
+                </div>
             </div>    
         );
     }
